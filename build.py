76c700be80acd4b3943ef696d21a74689a9b739b
dramos <dario.fenix@gmail.com>
2017-07-22 21:35:59 -0300

    DEV-8: Removed conan_test reference

--- build.py
+++ build.py
@@ -4,5 +4,5 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="dario-ramos", channel="testing")
     builder.password = os.getenv("CONAN_PASSWORD")
-    builder.add_common_builds(shared_option_name="conan_test:shared")
+    builder.add_common_builds()
     builder.run()
