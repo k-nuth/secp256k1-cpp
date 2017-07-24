101e858a4cfc1e77025f01f483ea1eae8d9a52d4
Dario Ramos <dario.fenix@gmail.com>
2017-07-24 14:44:49 -0300

    DEV-8: Move install code to install script (cleaner)

--- .travis/install.sh
+++ .travis/install.sh
@@ -19,6 +19,23 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv activate conan
 fi
 
+############################################################################
+# Install Guava
+############################################################################
+if [ ! -f $GUAVA_JAR ]; then wget $GUAVA_URL -O $GUAVA_JAR; fi
+############################################################################
+# All the dependencies are installed in /home/travis/deps/
+############################################################################
+DEPS_DIR="/home/travis/deps"
+mkdir ${DEPS_DIR} && cd ${DEPS_DIR}
+############################################################################
+# Install a recent CMake
+############################################################################
+CMAKE_URL="https://cmake.org/files/v3.9/cmake-3.9.0-rc5-Linux-x86_64.tar.gz"
+mkdir cmake && travis_retry wget --no-check-certificate --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
+echo 'Cmake 3.9.0 installed';
+export PATH=${DEPS_DIR}/cmake/bin:${PATH}
+
 
 pip install conan --upgrade
 pip install conan_package_tools
