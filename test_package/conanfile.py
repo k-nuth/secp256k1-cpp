88660b337bbd32fb3ad8ccc04fc8ec4d8a2e0147
Dario Ramos <dario.fenix@gmail.com>
2017-07-24 18:54:11 -0300

    DEV-8: Test

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -12,7 +12,7 @@ class Secp256k1TestConan(ConanFile):
     def build(self):
         cmake = CMake(self)
         # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is in "test_package"
-        cmake.configure(source_dir=self.conanfile_directory, build_dir="./")
+        cmake.configure(source_dir=self.conanfile_directory, build_dir="./", CMAKE_CXX_COMPILER_ID=settings["compiler.version"])
         cmake.build()
 
     def imports(self):
