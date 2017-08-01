b256f3c5ceadb3aab2f2d5f9a6199f2dfa5e4fba
dramos <dario.fenix@gmail.com>
2017-08-01 02:38:11 -0300

    DEV-8: Package only headers from the include folder

--- conanfile.py
+++ conanfile.py
@@ -19,7 +19,7 @@ class Secp256k1Conan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy("*.h", dst="include", src="src")
+        self.copy("*.h", dst="include", src="include", keep_path=True)
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.dylib*", dst="lib", keep_path=False)
