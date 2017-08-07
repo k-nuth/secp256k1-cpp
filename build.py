5edb5de9dd5856ebf17b58cd7d26ab1664bf7ba1
Dario Ramos <dario.fenix@gmail.com>
2017-08-07 16:42:37 -0300

    DEV-10: Appveyor build fix

--- build.py
+++ build.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     for settings, options, env_vars, build_requires in builder.builds:
         if settings["build_type"] == "Release" \
                 and settings["arch"] == "x86_64" \
-                and options["secp256k1:shared"] == False:
+                and not("secp256k1:shared" in options and options["secp256k1:shared"]):
             filtered_builds.append([settings, options, env_vars, build_requires])
 
     builder.builds = filtered_builds
