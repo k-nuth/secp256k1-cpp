c657c0c3529a4e952d61def737fa3a582e34a31e
Fernando pelliccioni <fpelliccioni@gmail.com>
2017-08-18 16:42:46 -0300

    fix

--- build.py
+++ build.py
@@ -1,5 +1,4 @@
 from conan.packager import ConanMultiPackager
-import os
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="bitprim", channel="stable",
@@ -7,21 +6,12 @@ if __name__ == "__main__":
                                  archs=["x86_64"])
 
     builder.add_common_builds(shared_option_name="secp256k1:shared")
-    # builder.password = os.getenv("CONAN_PASSWORD")
-
-    print("---------------------------------------")
-    print(builder.builds)
-    print("---------------------------------------")
 
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
         if settings["build_type"] == "Release" \
                 and not("secp256k1:shared" in options and options["secp256k1:shared"]):
             filtered_builds.append([settings, options, env_vars, build_requires])
-
-    print("---------------------------------------")
-    print(filtered_builds)
-    print("---------------------------------------")
 
     builder.builds = filtered_builds
     builder.run()
