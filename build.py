fee13cc20443ad1310fb9df8ba04ec9325cf2fb2
Fernando pelliccioni <fpelliccioni@gmail.com>
2018-02-01 16:02:09 -0300

    enable tests for GCC v7 on Linux

--- build.py
+++ build.py
@@ -19,7 +19,12 @@ if __name__ == "__main__":
                 and not("secp256k1:shared" in options and options["secp256k1:shared"]):
 
             env_vars["BITPRIM_BUILD_NUMBER"] = os.getenv('BITPRIM_BUILD_NUMBER', '-')
-                
+
+            if os.getenv('BITPRIM_RUN_TESTS', 'false') == 'true':
+                options["secp256k1:with_benchmark"] = "True"
+                options["secp256k1:with_tests"] = "True"
+                options["secp256k1:with_openssl_tests"] = "True"
+   
             filtered_builds.append([settings, options, env_vars, build_requires])
 
     builder.builds = filtered_builds
