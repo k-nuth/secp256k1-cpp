002dfbfded68ba301b36bc99a1fc1024141c6fe9
Fernando Pelliccioni <fpelliccioni@gmail.com>
2019-04-06 20:42:34 -0300

    helpers

--- helpers/clone-copy.py
+++ helpers/clone-copy.py
@@ -6,27 +6,27 @@ from shutil import copyfile
 PATH = '/Users/fernando/dev/bitprim/secp256k1/src/'
 SOURCE_PATH = '/Users/fernando/dev/bitcoin-abc/src/secp256k1/src/'
 
-# result = [os.path.join(dp, f) for dp, dn, filenames in os.walk(PATH) for f in filenames if os.path.splitext(f)[1] == '.txt']
-# result = [os.path.join(dp, f) for dp, dn, filenames in os.walk(PATH) for f in filenames]
-result1 = [(dp, f) for dp, dn, filenames in os.walk(PATH) for f in filenames]
+# # result = [os.path.join(dp, f) for dp, dn, filenames in os.walk(PATH) for f in filenames if os.path.splitext(f)[1] == '.txt']
+# # result = [os.path.join(dp, f) for dp, dn, filenames in os.walk(PATH) for f in filenames]
+# result1 = [(dp, f) for dp, dn, filenames in os.walk(PATH) for f in filenames]
 
-for dp, f in result1:
-    x = os.path.join(dp, f)
+# for dp, f in result1:
+#     x = os.path.join(dp, f)
 
-    # print(dp)
-    # print(x)
-    # print(x[len(PATH):])
+#     # print(dp)
+#     # print(x)
+#     # print(x[len(PATH):])
 
-    source_file = os.path.join(SOURCE_PATH, x[len(PATH):])   
-    print(source_file)
-    # print(os.path.isfile(source_file))
+#     source_file = os.path.join(SOURCE_PATH, x[len(PATH):])   
+#     print(source_file)
+#     # print(os.path.isfile(source_file))
 
-    # print(x)
+#     # print(x)
 
-    try:
-        copyfile(source_file, x)
-    except IOError as identifier:
-        pass
+#     try:
+#         copyfile(source_file, x)
+#     except IOError as identifier:
+#         pass
 
 print('------------------------------------------------')
 
@@ -43,11 +43,21 @@ for dp, f in result2:
     print(target_file)
     # print(os.path.isfile(source_file))
 
-    # print(x)
+    dir_name, _ = os.path.split(target_file)
+    print(dir_name)
 
     try:
-        copyfile(x, target_file)
-    except IOError as identifier:
+        os.makedirs(dir_name) #, exist_ok=True)
+    except OSError as identifier:
         pass
+
+
+    # print(x)
+    copyfile(x, target_file)
+
+    # try:
+    #     copyfile(x, target_file)
+    # except IOError as identifier:
+    #     pass
 
     
