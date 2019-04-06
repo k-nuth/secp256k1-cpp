3fc53063f3c03e66ef7892cde61ab32e9b0eae49
Fernando Pelliccioni <fpelliccioni@gmail.com>
2019-04-06 20:31:33 -0300

    helpers

--- helpers/clone-copy.py
+++ helpers/clone-copy.py
@@ -3,8 +3,8 @@ import os.path
 from shutil import copyfile
 
 
-PATH = '/Users/fernando/dev/bitprim/bitprim-consensus/src/clone-abc/'
-SOURCE_PATH = '/Users/fernando/dev/bitcoin-abc/src/'
+PATH = '/Users/fernando/dev/bitprim/secp256k1/src/'
+SOURCE_PATH = '/Users/fernando/dev/bitcoin-abc/src/secp256k1/src/'
 
 # result = [os.path.join(dp, f) for dp, dn, filenames in os.walk(PATH) for f in filenames if os.path.splitext(f)[1] == '.txt']
 # result = [os.path.join(dp, f) for dp, dn, filenames in os.walk(PATH) for f in filenames]
